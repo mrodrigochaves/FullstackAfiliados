204caabaea48898bb7d98fae646bc5faf01d9ee5
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-30 21:41:40 -0300

    update repository

--- backend/src/main/java/com/mrodrigochaves/backend/repository/TransactionRepository.java
+++ backend/src/main/java/com/mrodrigochaves/backend/repository/TransactionRepository.java
@@ -1,11 +1,8 @@
 package com.mrodrigochaves.backend.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.mrodrigochaves.backend.model.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
-
-
+   
     
 }
