a7d0df3b00a7ee7fa6173225f3d4e8d6e12c93cc
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-02 20:24:11 -0300

    merge controllers

--- backend/src/main/java/com/mrodrigochaves/backend/controller/TransactionUploadController.java
+++ backend/src/main/java/com/mrodrigochaves/backend/controller/TransactionUploadController.java
@@ -5,19 +5,37 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.mrodrigochaves.backend.model.Transaction;
+import com.mrodrigochaves.backend.repository.TransactionRepository;
 import com.mrodrigochaves.backend.service.TransactionService;
 
 @Controller
-public class UploadFileController {
-    
+// CORS configuration
+@CrossOrigin(origins = "http://localhost:8080")
+// Path
+@RequestMapping("/api")
+public class TransactionUploadController {
+
+    private final TransactionRepository transactionRepository;
+    private final TransactionService transactionService;
+
     @Autowired
-    private TransactionService transactionService;
+    public TransactionUploadController(TransactionRepository transactionRepository, TransactionService transactionService) {
+        this.transactionRepository = transactionRepository;
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public List<Transaction> list() {
+        return transactionRepository.findAll();
+    }
 
     @GetMapping("/upload")
     public String showUploadForm() {
