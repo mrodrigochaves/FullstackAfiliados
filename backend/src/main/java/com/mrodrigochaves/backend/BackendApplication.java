cd02adef1d663dab4c39fa5aead5de00137bb564
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-01 22:56:33 -0300

    add comments

--- backend/src/main/java/com/mrodrigochaves/backend/BackendApplication.java
+++ backend/src/main/java/com/mrodrigochaves/backend/BackendApplication.java
@@ -13,6 +13,7 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    // CORS configuration
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
