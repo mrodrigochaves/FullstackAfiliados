f565f6e3c7c225bbb9ed4615a3952a430dacb6e1
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-04 19:59:01 -0300

    update app routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'table', loadChildren: () => import('./component/table/table.module').then(m => m.TableModule) },
-  { path: 'upload-form', loadChildren: () => import('./component/upload-form/upload-form.module').then(m => m.UploadFormModule) },
+  { path:'', pathMatch:'full', redirectTo: 'home'},
+  { path:'home',
+  loadChildren:() => import('./home/home.component').then(m => m.HomeComponent)
+}
 ];
 
 @NgModule({
