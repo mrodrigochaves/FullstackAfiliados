b85dc8f1e2570fd450ee4754fc44aae500b16db2
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-05 20:34:07 -0300

    update routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path:'', pathMatch:'full', redirectTo: 'home'},
   { path:'home',
-  loadChildren:() => import('./home/home.component').then(m => m.HomeComponent)
+  loadChildren:() => import('./component/home/home.component').then(m => m.HomeComponent)
 }
 ];
 
