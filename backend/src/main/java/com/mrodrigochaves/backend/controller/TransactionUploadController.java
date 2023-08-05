1f36bc1fd71dc90e9179eab37b14e9bf9b46574d
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-05 18:40:48 -0300

    change anotation for RestController

--- backend/src/main/java/com/mrodrigochaves/backend/controller/TransactionUploadController.java
+++ backend/src/main/java/com/mrodrigochaves/backend/controller/TransactionUploadController.java
@@ -4,19 +4,19 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.mrodrigochaves.backend.model.Transaction;
 import com.mrodrigochaves.backend.repository.TransactionRepository;
 import com.mrodrigochaves.backend.service.TransactionService;
 
-@Controller
+@RestController
 // CORS configuration
 @CrossOrigin(origins = "http://localhost:8080")
 // Path
