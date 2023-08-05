14e5df8f529728455d32db28cfa0783182682bed
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-05 20:34:53 -0300

    update app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,45 +4,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApiService } from './services/api.service';
-import { TableComponent } from './component/table/table.component';
 import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-import { UploadFormComponent } from './resources/templates/upload-form/upload-form.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { TableModule } from './component/table/table.module';
-import { UploadFormModule } from './resources/templates/upload-form/upload-form.module';
-import { HomeComponent } from './home/home.component';
-import {MatInputModule} from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SharedModule } from 'src/shared/shared.module';
+import { HomeComponent } from './component/home/home.component';
+import { TableComponent } from './component/table/table.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        TableComponent,
-        UploadFormComponent,
-        HomeComponent
+  declarations: [AppComponent, HomeComponent, TableComponent],
+  providers: [ApiService],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    SharedModule,
+    MatPaginatorModule
     ],
-    providers: [
-      ApiService,
-    ],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        MatTableModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        MatPaginatorModule,
-        FormsModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatSnackBarModule,
-        TableModule,
-        UploadFormModule,
-        MatInputModule
-    ]
 })
-export class AppModule { }
+export class AppModule {}
