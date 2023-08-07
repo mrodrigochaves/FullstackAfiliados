b011b6442d72561c753734fd0ddf5f18208e2560
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-06 21:06:40 -0300

    update routing app

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  { path:'', pathMatch:'full', redirectTo: 'home'},
+  { path:'home',
+  loadChildren:() => import('./trasaction/containers/home/home.component').then(m => m.HomeComponent)
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
