39123d33c2f0624aa2f95c57c35f59019c9ac98a
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-08 15:55:17 -0300

    create user dto

---
+++ backend/src/main/java/com/mrodrigochaves/backend/dto/UsersDTO.java
@@ -0,0 +1,19 @@
+package com.mrodrigochaves.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class UsersDTO {
+    
+
+    private String username;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+}
