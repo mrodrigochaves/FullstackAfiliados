2c7730909d4e400e7cfe0f6e875d0eec33760a49
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-07 21:08:50 -0300

    update table

--- frontend/src/app/trasaction/component/table/table.component.ts
+++ frontend/src/app/trasaction/component/table/table.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { Transaction } from 'src/app/trasaction/model/transactions';
 import { ApiService } from 'src/app/trasaction/services/api.service';
+import { Transaction } from 'src/app/trasaction/model/transactions';
+import { ErrorDialogService } from '../../../shared/containers/error-dialog/error-dialog.service';
 
 @Component({
   selector: 'app-table',
@@ -13,17 +14,21 @@ export class TableComponent implements OnInit {
   dataSource: MatTableDataSource<Transaction>;
   displayedColumns: string[] = ['id', 'type_id', 'date', 'product', 'price', 'seller'];
 
-  @ViewChild('paginator')
-  paginator!: MatPaginator;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private apiService: ApiService) {
+  constructor(private apiService: ApiService, private errorDialogService: ErrorDialogService) {
     this.dataSource = new MatTableDataSource<Transaction>();
   }
 
   ngOnInit() {
-    this.apiService.getTransactions().subscribe((data: Transaction[]) => {
-      this.dataSource.data = data;
-      this.dataSource.paginator = this.paginator;
-    });
+    this.apiService.getTransactions().subscribe(
+      (data: Transaction[]) => {
+        this.dataSource.data = data;
+        this.dataSource.paginator = this.paginator;
+      },
+      (error) => {
+        this.errorDialogService.openDialog('Failed to load transactions.');
+      }
+    );
   }
 }
