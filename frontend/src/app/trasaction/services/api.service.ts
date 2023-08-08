3cd408c20d0a947b070ea20cb63ef852df12f1d2
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-07 21:09:21 -0300

    testing api

--- frontend/src/app/trasaction/services/api.service.ts
+++ frontend/src/app/trasaction/services/api.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Transaction } from '../model/transactions';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  errorDialogService: any;
 
   uploadFile(formData: FormData): Observable<any> {
     return this.http.post<any>(this.apiUrl + 'upload', formData);
   }
 
-  private apiUrl = 'http://localhost:8080/api';
+  private apiUrl = 'http://localhost:8080/ap';
 
   constructor(private http: HttpClient) {}
 
   getTransactions(): Observable<Transaction[]> {
-    return this.http.get<Transaction[]>(this.apiUrl);
+    return this.http.get<Transaction[]>(this.apiUrl).pipe(
+      catchError((error) => {
+        this.errorDialogService.openDialog('Failed to load transactions.');
+        throw error;
+      })
+    );
   }
 }
