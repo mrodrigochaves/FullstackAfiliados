70caecd5385e60d0b560412a8aad775b97316ada
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-08 15:46:50 -0300

    add bean encrypto

--- backend/src/main/java/com/mrodrigochaves/backend/BackendApplication.java
+++ backend/src/main/java/com/mrodrigochaves/backend/BackendApplication.java
@@ -3,6 +3,7 @@ package com.mrodrigochaves.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,12 +20,17 @@ public class BackendApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") 
-                        .allowedOrigins("http://localhost:4200") 
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") 
-                        .allowedHeaders("*") 
-                        .allowCredentials(true); 
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
