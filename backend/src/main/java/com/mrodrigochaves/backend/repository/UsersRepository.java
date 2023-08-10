210d8d9c0c55b56361dea0223ef8b894981993db
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-09 21:16:05 -0300

    update user repository

--- backend/src/main/java/com/mrodrigochaves/backend/repository/UsersRepository.java
+++ backend/src/main/java/com/mrodrigochaves/backend/repository/UsersRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mrodrigochaves.backend.model.Users;
 
 public interface UsersRepository extends JpaRepository<Users, String> {
-    Users findByUsersName(String username);
+    Users findByUsername(String username);
 }
