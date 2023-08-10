696acabd7b44168f5914fffb4c8ae025dd2851f1
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-09 21:14:08 -0300

    create user controller

--- backend/src/main/java/com/mrodrigochaves/backend/controller/UsersController.java
+++ backend/src/main/java/com/mrodrigochaves/backend/controller/UsersController.java
@@ -3,10 +3,12 @@ package com.mrodrigochaves.backend.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mrodrigochaves.backend.model.Transaction;
+import com.mrodrigochaves.backend.model.Users;
 import com.mrodrigochaves.backend.service.UsersService;
 
 @RestController
@@ -20,8 +22,13 @@ public class UsersController {
     }
     
     @GetMapping("/users/all")
-    public List<Transaction> getAllUsers(){
+    public List<Users> getAllUsers(){
         return usersService.getAll();
             
+    }
+
+    @PostMapping("/users/create") 
+    public Users createUser(@RequestBody Users users) {
+        return usersService.createUser(users);
     }
 }
